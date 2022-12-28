dbde5d18f519a648ca02011c633d1b56002122b4
Jhinger <gurshanjhinger22@gmail.com>
2022-12-28 12:18:51 -0800

    refactor: move positioning css outside of navabar elements for component reusability

--- components/Button/SignIn/signin.tsx
+++ components/Button/SignIn/signin.tsx
@@ -6,11 +6,9 @@ interface ISignInProps {
 
 const SignIn = ({ onClickTask }: ISignInProps) => {
     return (
-        <div className="relative mr-8">
-            <button onClick={onClickTask} className="unselectable ring-2 ring-offset-2 ring-offset-secondary ring-primaryAccent hover:ring-white bg-primary transition duration-150 text-secondary font-semibold py-2 px-4 rounded text-sm">
-                Sign In
-            </button>
-        </div>
+        <button onClick={onClickTask} className="unselectable ring-2 ring-offset-2 ring-offset-secondary ring-primaryAccent hover:ring-white bg-primary transition duration-150 text-secondary font-semibold py-2 px-4 rounded text-sm">
+            Sign In
+        </button>
     )
 }
 
