3ee2725186238a48f8f9f7d22e2b0ed1d4499e42
Jhinger <gurshanjhinger22@gmail.com>
2022-12-30 17:24:36 -0800

    styles: profile tray updated - temporary solution for delayed second tray item.

--- components/Button/Profile/profile.tsx
+++ components/Button/Profile/profile.tsx
@@ -1,5 +1,6 @@
 import React, { useCallback, useRef, useState } from "react";
 import Image from "next/image";
+
 import defaultuser from "@/static/defaultuser.png"
 import Tray from "@/components/Tray";
 import TrayItem from "@/components/TrayItem";
@@ -46,11 +47,13 @@ const Profile = ({ session, className = "", onClickTask }: IProfileProps) => {
                 alt={session?.user?.name || "Profile"}
             />
 
-            <Tray onClick={stopPropagation} isVisible={isTrayVisible} className="absolute top-full mt-2">
-                <TrayItem>Testing</TrayItem>
-                <TrayItem>Another</TrayItem>
-                <TrayItem>Component</TrayItem>
-                <TrayItem>Created</TrayItem>
+            <Tray onClick={stopPropagation} isVisible={isTrayVisible} className="absolute top-full right-0 mt-2">
+                <TrayItem className="animate-fadeDown bg-secondary text-primary text-sm px-4 py-2 my-1.5 font-bold hover:text-white duration-200 ring-2 ring-primary ring-offset-2 ring-offset-secondary hover:ring-white">
+                    My Profile
+                </TrayItem>
+                <TrayItem onClick={onClickTask} className="animate-fadeDownDelay bg-secondary text-primary text-sm px-4 py-2 my-1.5 font-bold hover:text-red-300 duration-200 ring-2 ring-primary ring-offset-2 ring-offset-secondary hover:ring-red-300">
+                    Log Out
+                </TrayItem>
             </Tray>
         </div>
     )
--- pages/index.tsx
+++ pages/index.tsx
@@ -8,7 +8,7 @@ export default function Home() {
 				<title>RateMyClass</title>
 				<meta name="RateMyClass" content="RateMyClass is an informational hub for University and College students looking for student-based feedback regarding their classes." />
 				<meta name="viewport" content="width=device-width, initial-scale=1" />
-				<link rel="icon" href="/favicon.ico" />
+				<link rel="icon" href="/static/logo-2.svg"/>
 			</Head>
 			<Hero />
 		</>
