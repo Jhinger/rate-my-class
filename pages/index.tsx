225d679b291fb0330fea8130c87864ef50eb4e45
Jhinger <gurshanjhinger22@gmail.com>
2022-12-30 22:44:01 -0800

    style: hero section, still working with colors to see what works best

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,7 +8,7 @@ export default function Home() {
 				<title>RateMyClass</title>
 				<meta name="RateMyClass" content="RateMyClass is an informational hub for University and College students looking for student-based feedback regarding their classes." />
 				<meta name="viewport" content="width=device-width, initial-scale=1" />
-				<link rel="icon" href="/static/logo-2.svg"/>
+				<link rel="icon" href="/static/logo-2.svg" sizes='16x16'/>
 			</Head>
 			<Hero />
 		</>
