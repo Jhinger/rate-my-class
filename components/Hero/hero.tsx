56ec257b2e990b1220bf74f09e4ec214033ab61e
Jhinger <gurshanjhinger22@gmail.com>
2022-12-30 22:44:01 -0800

    style: hero section, still working with colors to see what works best

--- components/Hero/hero.tsx
+++ components/Hero/hero.tsx
@@ -1,6 +1,5 @@
-import Image from "next/image"
-
-import shapeleft from "@/static/shapeleft.svg"
+import Image from 'next/image';
+import logo from '@/static/logo-hero.svg'
 
 interface IHeroProps {
     title?: string;
@@ -14,13 +13,25 @@ const Hero = ({
         children
     }: IHeroProps) => {
     return (
-        <header className="w-full h-[40rem] border-2 border-red-500 border-solid">
-            <div className="" />
-            <div className="" />
-
-            <h1 className=""> { title } </h1>
-            <h4 className=""> { subtitle } </h4>
-            <div className="">
+        <header className="w-full h-[40rem] flex flex-col justify-center items-center border-2 border-red-500 border-solid">
+            <div className="flex flex-col justify-center items-center relative -top-10">
+                <div className="flex flex-row items-center">
+                    <Image
+                        className="unselectable"
+                        src={logo}
+                        width={40}
+                        height={30}
+                        quality={100}
+                        alt=""
+                        priority={true}
+                    />
+                    <h1 className="p-2 font-extrabold text-8xl tracking-tightest bg-gradient-to-r bg-clip-text text-transparent from-primary to-tertiary animate-gradient"> 
+                        { title } 
+                    </h1>
+                </div>
+                <h4 className="text-white font-extralight"> { subtitle } </h4>
+            </div>
+            <div className="search-bar-passed-in-as-child">
                 { children }
             </div>
         </header>
