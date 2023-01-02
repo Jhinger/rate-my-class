c397abe986ec6db5f3a86160add11bc76af40db6
Jhinger <gurshanjhinger22@gmail.com>
2023-01-01 20:03:45 -0800

    feat: features section and card component shells

---
+++ components/Overview/overview.tsx
@@ -0,0 +1,10 @@
+
+const Overview =() => {
+    return (
+        <div className="w-full h-[40rem] bg-secondary border-2 border-solid border-green-500">
+
+        </div>
+    )
+}
+
+export default Overview;
