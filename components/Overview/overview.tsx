337fb44942a8aeec97069b94cffe3e8f16fe7750
Jhinger <gurshanjhinger22@gmail.com>
2023-01-04 21:18:13 -0800

    feat: added school carousel

--- components/Overview/overview.tsx
+++ components/Overview/overview.tsx
@@ -1,8 +1,10 @@
+import Carousel from "@/components/Carousel"
+import schoolsImagesArr from "@/static/schools";
 
 const Overview =() => {
     return (
         <div className="w-full h-[40rem] bg-secondary border-2 border-solid border-green-500">
-
+            <Carousel schools={schoolsImagesArr} />
         </div>
     )
 }
---
+++ public/static/schools/index.ts
@@ -0,0 +1,19 @@
+import ucla from './ucla.svg'
+import nyu from "./nyu.svg"
+import yale from './yale.svg'
+import ubc from "./ubc.svg"
+import ucirvine from "./ucirvine.svg"
+import mit from './mit.svg'
+import waterloo from "./waterloo.svg"
+
+const schoolsImagesArr = [
+    ucla,
+    nyu,
+    yale,
+    ucirvine,
+    ubc,
+    mit,
+    waterloo
+];
+
+export default schoolsImagesArr;
