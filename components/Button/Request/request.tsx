b02d98890927c1a899d1add4a4768c1da4f7df62
Jhinger <gurshanjhinger22@gmail.com>
2023-01-05 17:32:14 -0800

    feat: request dropdown - still needs to be hooked up to submit function

--- components/Button/Request/request.tsx
+++ components/Button/Request/request.tsx
@@ -2,17 +2,38 @@ import { useState } from "react";
 
 const Request = () => {
     const [isOpen, setIsOpen] = useState(false);
+    const [request, setRequest] = useState("");
+
+    const updateFormVisibility = () => {
+        setIsOpen(!isOpen);
+    }
+
+    const updateRequest = (newValue: string) => {
+        setRequest(newValue);
+    }
+
+    const stopPropagation = (event: React.MouseEvent<HTMLFormElement>)  => {
+        event.stopPropagation();
+    }
 
     return (
-        <div className="flex flex-col">
+        <div className="flex flex-col justify-center items-center" onClick={updateFormVisibility}>
             <button className="font-semibold bg-primary rounded-md px-24 py-4 hover:bg-tertiaryAccent duration-150">
                 REQUEST A SCHOOL / CLASS
             </button>
             {isOpen && 
-                <div className="">
-                    <span>Enter a school and optionally a class:</span>
-                    <input type="text" placeholder="ex. Stanford - CS107" />
-                </div>
+                <form onClick={stopPropagation} className="flex flex-col bg-primary p-2 m-2 w-full rounded-md">
+                    <label htmlFor="request" className="m-2 font-extralight text-sm">Enter a school and optionally a class:</label>
+                    <input 
+                        id="request" 
+                        className="rounded-sm p-2 m-2"
+                        value={request} 
+                        type="text" 
+                        placeholder="ex. Stanford - CS107" 
+                        onChange={(e) => updateRequest(e.target.value)}
+                        autoFocus
+                    />
+                </form>
             }
         </div>
     )
