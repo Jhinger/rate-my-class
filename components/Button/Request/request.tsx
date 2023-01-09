aad81acf67d9089c1c7f94b57ca5e5cac9430729
Jhinger <gurshanjhinger22@gmail.com>
2023-01-08 16:55:43 -0800

    feat: bootstrap searchbar and reorder attribute order

--- components/Button/Request/request.tsx
+++ components/Button/Request/request.tsx
@@ -18,20 +18,20 @@ const Request = () => {
 
     return (
         <div className="flex flex-col justify-center items-center" onClick={updateFormVisibility}>
-            <button className="font-semibold bg-primary rounded-md px-24 py-4 hover:bg-tertiaryAccent duration-150">
+            <button className="font-semibold bg-primary rounded-md px-24 py-4 hover:bg-white duration-150">
                 REQUEST A SCHOOL / CLASS
             </button>
             {isOpen && 
                 <form onClick={stopPropagation} className="flex flex-col bg-primary p-2 m-2 w-full rounded-md">
                     <label htmlFor="request" className="m-2 font-extralight text-sm">Enter a school and optionally a class:</label>
                     <input 
+                        autoFocus
                         id="request" 
                         className="rounded-sm p-2 m-2"
-                        value={request} 
                         type="text" 
                         placeholder="ex. Stanford - CS107" 
+                        value={request} 
                         onChange={(e) => updateRequest(e.target.value)}
-                        autoFocus
                     />
                 </form>
             }
---
+++ components/Searchbar/searchbar.tsx
@@ -0,0 +1,14 @@
+
+interface ISearchBarProps {
+    options: any[];
+}
+
+const SearchBar = ({ options }: ISearchBarProps) => {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default SearchBar;
