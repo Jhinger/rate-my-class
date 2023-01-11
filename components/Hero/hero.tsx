1e2543de8c12198e86efd0b90f4a0ec5679df7f4
Jhinger <gurshanjhinger22@gmail.com>
2023-01-10 18:28:37 -0800

    style: added searchbar styles

--- components/Hero/hero.tsx
+++ components/Hero/hero.tsx
@@ -15,25 +15,30 @@ const Hero = ({
     }: IHeroProps) => {
     return (
         <header className="w-full h-[40rem] flex flex-col justify-center items-center">
-            <div className="flex flex-col justify-center items-center relative -top-10">
-                <div className="flex flex-row items-center">
-                    <Image
-                        className="unselectable"
-                        src={logo}
-                        width={40}
-                        height={30}
-                        quality={100}
-                        alt=""
-                        priority={true}
-                    />
-                    <h1 className="p-2 font-extrabold text-8xl tracking-tightest bg-gradient-to-r bg-clip-text text-transparent from-primary via-tertiaryAccent to-tertiary animate-gradient"> 
-                        { title } 
-                    </h1>
+            <div className="relative -top-10">
+                <div className="flex flex-col justify-center items-center">
+                    <div className="flex flex-row items-center">
+                        <Image
+                            className="unselectable"
+                            src={logo}
+                            width={40}
+                            height={30}
+                            quality={100}
+                            alt=""
+                            priority={true}
+                        />
+                        <h1 className="p-2 font-extrabold text-8xl tracking-tightest bg-gradient-to-r bg-clip-text text-transparent from-primary via-tertiaryAccent to-tertiary animate-gradient"> 
+                            { title } 
+                        </h1>
+                    </div>
+                    <h4 className="text-white font-light mt-4"> { subtitle } </h4>
                 </div>
-                <h4 className="text-white font-extralight"> { subtitle } </h4>
-            </div>
-            <div className="search-bar-passed-in-as-child">
-                { children }
+                <div className="m-4">
+                    { children }
+                </div>
+                <div className="text-white text-sm font-extralight text-center">
+                    Can&lsquo;t find your school? Request it to be <span className='text-blue-400 hover:text-tertiary hover:cursor-pointer'>added here</span>.
+                </div>
             </div>
         </header>
     )
--- components/Searchbar/searchbar.tsx
+++ components/Searchbar/searchbar.tsx
@@ -6,7 +6,11 @@ interface ISearchBarProps {
 const SearchBar = ({ options }: ISearchBarProps) => {
     return (
         <div>
-
+            <input 
+                type="text" 
+                className="w-[60rem] px-4 py-2 bg-white rounded-md ring-2 ring-primary ring-offset-2 ring-offset-secondary hover:ring-tertiary duration-100 focus:outline-none" 
+                placeholder={`ex. 'Simon Fraser University' or 'SFU'`}
+            />
         </div>
     )
 }
