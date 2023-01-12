cf0e09de2212206aebbc54d68999a092d4a8c1b7
Jhinger <gurshanjhinger22@gmail.com>
2023-01-11 22:06:35 -0800

    feat: redirect to request class button on link under searchbar

--- components/Searchbar/searchbar.tsx
+++ components/Searchbar/searchbar.tsx
@@ -7,6 +7,7 @@ const SearchBar = ({ options }: ISearchBarProps) => {
     return (
         <div>
             <input 
+                autoFocus
                 type="text" 
                 className="w-[60rem] px-4 py-2 bg-white rounded-md ring-2 ring-primary ring-offset-2 ring-offset-secondary hover:ring-tertiary duration-100 focus:outline-none" 
                 placeholder={`ex. 'Simon Fraser University' or 'SFU'`}
