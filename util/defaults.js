9e2670d34abf27589a8ebb921febd047034636ee
Jhinger <gurshanjhinger22@gmail.com>
2023-01-12 19:22:45 -0800

    feat: keypress hook and constants for navigating searchbar with arrow keys

---
+++ util/defaults.js
@@ -0,0 +1,15 @@
+
+const defaultOptions = [
+    {
+        name: "Simon Fraser University",
+        short: "SFU",
+    },
+    {
+        name: "University of British Columbia",
+        short: "UBC",
+    }
+];
+
+export {
+    defaultOptions
+}
