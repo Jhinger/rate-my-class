c8a6b0d39a7086ae6ff1b924cbf2e254d063e284
Jhinger <gurshanjhinger22@gmail.com>
2023-01-14 19:47:37 -0800

    feat: functioning searchbar filter, adding more generic types

--- types/index.ts
+++ types/index.ts
@@ -1,4 +1,12 @@
 
+export type PropertyKey = string | number | symbol;
+
 export type Maybe<T> = T | null | undefined;
 
 export type StatusOptions = "loading" | "authenticated" | "unauthenticated";
+
+export type ValueOfType<T> = T[keyof T];
+
+export type UntypedObject = {
+    [key: PropertyKey]: any;
+}
