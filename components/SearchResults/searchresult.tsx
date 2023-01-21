04670f95a309506c80833bf483c6be9ae272f9b0
Jhinger <gurshanjhinger22@gmail.com>
2023-01-20 20:17:09 -0800

    style: search bar results dropdown styles applied - possibly temporary

--- components/SearchResults/searchresult.tsx
+++ components/SearchResults/searchresult.tsx
@@ -5,20 +5,22 @@ interface ISearchResultProps {
     className?: string;
 }
 
-const SearchResult = ({ results, className }: ISearchResultProps) => {
+const SearchResult = ({ results, className, ...rest }: ISearchResultProps) => {
 
     const renderResults = results.map((result, index) => 
-        <div key={index}>
-            { result.name ?? "Could not render" }
+        <div key={index} {...rest} className="duration-100 px-2 py-3 indent-2 text-gray-500 hover:text-black hover:cursor-pointer hover:bg-gray-200 hover:rounded-md text-sm" >
+            { result.name }
         </div>
     )
 
     return (
-        <div className={`${className} `}>
+        <>
             {results.length !== 0 && 
-                renderResults
+                <div className={`${className} w-full bg-white mt-4 rounded-md ring-2 ring-primary ring-offset-2 ring-offset-secondary hover:ring-tertiary`}>
+                    { renderResults } 
+                </div>
             }
-        </div>
+        </>
     )
 }
 
