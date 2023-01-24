9cc3aa668995818ea48d550d10cb6b50d01f098e
Jhinger <gurshanjhinger22@gmail.com>
2023-01-23 20:43:59 -0800

    feat: adding school and class routes

---
+++ pages/[school]/index.tsx
@@ -0,0 +1,21 @@
+import { GetServerSidePropsContext, InferGetServerSidePropsType, PreviewData } from "next";
+import { ParsedUrlQuery } from "querystring";
+
+const SchoolIndex = ({ school }: InferGetServerSidePropsType<typeof getServerSideProps>) => {
+    return (
+        <div>
+            { school } 
+        </div>
+    )
+}
+
+export default SchoolIndex;
+
+export async function getServerSideProps<Q extends ParsedUrlQuery, D extends PreviewData>(context: GetServerSidePropsContext<Q, D>,) {
+    const params = context.params!;
+    const school = params.school;
+
+    return {
+        props: { school }
+    }
+}
