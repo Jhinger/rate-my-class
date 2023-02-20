55f079ed9c8cb04d5d936aa956e78b8486b3311f
Jhinger <gurshanjhinger22@gmail.com>
2023-02-20 14:55:09 -0800

    fix: possible solution to phantom .short undefined error, hover was setting cursor to -1

--- components/Carousel/carousel.tsx
+++ components/Carousel/carousel.tsx
@@ -10,7 +10,7 @@ const Carousel = ({ schools }: ICarouselProps) => {
 
     const renderImages = schools.map((school, i) => (
         <div key={i} className="slide flex justify-center align-center">
-            <Image src={school} width={50} height={75} alt={''} quality={100} className="unselectable" loading="lazy" />
+            <Image src={school} style={{height: "auto", width:"60%"}} alt={''} quality={100} className="unselectable" loading="lazy" />
         </div>
     ))
 
