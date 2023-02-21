c4351e7fb2ce7635c93f535dc8f86dd0daefe4c2
Jhinger <gurshanjhinger22@gmail.com>
2023-02-20 18:25:46 -0800

    refactor: update schema and test different methods for average grade values

--- components/Card/card.tsx
+++ components/Card/card.tsx
@@ -1,7 +1,7 @@
 
 const Card = () => {
     return (
-        <div className="w-[20rem] aspect-square bg-primary mx-8 rounded-3xl">
+        <div className="w-[25rem] h-[20rem] bg-primary mx-6 rounded-3xl">
 
         </div>
     )
