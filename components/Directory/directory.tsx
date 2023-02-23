d1846233576503eaf6c0df63813f76386a2f502c
Jhinger <gurshanjhinger22@gmail.com>
2023-02-22 19:08:07 -0800

    feat: template components for department directory

---
+++ components/Directory/directory.tsx
@@ -0,0 +1,15 @@
+import { DepartmentSummary } from '@/types/'
+
+interface IDirectoryProps {
+    summary: DepartmentSummary[];
+}
+
+const Directory = ({ summary }: IDirectoryProps) => {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default Directory;
--- components/EmptyState/emptystate.tsx
+++ components/EmptyState/emptystate.tsx
@@ -1,5 +1,11 @@
 
-const EmptyState = () => {
+interface IEmptyStateProps {
+    title: string;
+    subtitle?: string;
+    callToAction?: () => unknown;
+}
+
+const EmptyState = ({ title, subtitle, callToAction }: IEmptyStateProps) => {
     return (
         <div>
             Empty State.
