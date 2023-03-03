cc51e68828bef8b6f7d718d168a3bda7ac3d2dd3
Jhinger <gurshanjhinger22@gmail.com>
2023-03-02 22:44:39 -0800

    feat: first barchart added, need to style

--- components/Chart/chart.tsx
+++ components/Chart/chart.tsx
@@ -1,15 +1,17 @@
 import BarChart from '@/components/Chart/BarChart/';
 import PieChart from '@/components/Chart/PieChart/';
-import type { ChartType } from '@/types/'
+
+import type { ChartType, UntypedObject } from '@/types/'
 interface IChartProps {
     type: ChartType;
+    data: UntypedObject[];
     classes?: string;
 }
 
-const Chart = ({ type, classes }: IChartProps) => {
+const Chart = ({ type, data, classes }: IChartProps) => {
     return (
         <div className={`${classes} flex justify-center items-center m-4 w-1/2 h-[20rem] bg-primary rounded-md`}>
-            {type==="barchart" && <BarChart />}
+            {type==="barchart" && <BarChart label={"Highest Rated GPA Boosters"} barData={data} />}
             {type==="piechart" && <PieChart />}
         </div>
     )
