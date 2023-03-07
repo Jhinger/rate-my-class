6920ee4269ab8f4952b85adf5956dfd4f6f72b56
Jhinger <gurshanjhinger22@gmail.com>
2023-03-06 17:21:43 -0800

    feat: update chart colors

---
+++ constants/boosterColors.ts
@@ -0,0 +1,25 @@
+export const colors_blue = [
+    '#003d4d',
+    '#005266',
+    '#006680',
+    '#007a99',
+    '#008fb3',
+    '#00a3cc',
+    '#00b8e6',
+    '#00ccff',
+    '#1ad1ff',
+    '#33d6ff'
+]
+
+export const colors_mauve = [
+    '#7a179b',
+    '#8c1bb1',
+    '#9d1ec8',
+    '#af21de',
+    '#b737e1',
+    '#bf4ee4',
+    '#c764e8',
+    '#cf7aeb',
+    '#d790ee',
+    '#dfa6f2'
+];
