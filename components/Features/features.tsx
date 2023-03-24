7b1bb6844f253011a839a0f558a7e5fd7bddb158
Jhinger <gurshanjhinger22@gmail.com>
2023-03-23 21:56:03 -0700

    refactor: landing page cards

--- components/Features/features.tsx
+++ components/Features/features.tsx
@@ -2,16 +2,13 @@ import Card from "@/components/Card"
 import { cards } from "@/util/getCardProps";
 
 const Features = () => {
-
-    const renderCards = cards.map((card, index) => (
-        <div key={index}>
-            <Card imgSrc={card.imgSrc} title={card.title} subtext={card.subtext} />
-        </div>
-    ))
+    const [ card1, card2, card3 ] = cards;
 
     return (
         <div className="w-full h-[35rem] bg-secondary flex flex-row justify-center items-center">
-            { renderCards }
+            <Card imgSrc={card1.imgSrc} title={card1.title} subtext={card1.subtext} className="" />
+            <Card imgSrc={card2.imgSrc} title={card2.title} subtext={card2.subtext} className="" />
+            <Card imgSrc={card3.imgSrc} title={card3.title} subtext={card3.subtext} className="" />
         </div>
     )
 }
