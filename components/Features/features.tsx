4ddb133a8f1a25518c343f1edd5d0038bfd429a5
Jhinger <gurshanjhinger22@gmail.com>
2023-03-23 21:32:37 -0700

    feat: adding landing page cards

--- components/Features/features.tsx
+++ components/Features/features.tsx
@@ -1,11 +1,17 @@
 import Card from "@/components/Card"
+import { cards } from "@/util/getCardProps";
 
 const Features = () => {
+
+    const renderCards = cards.map((card, index) => (
+        <div key={index}>
+            <Card imgSrc={card.imgSrc} title={card.title} subtext={card.subtext} />
+        </div>
+    ))
+
     return (
         <div className="w-full h-[35rem] bg-secondary flex flex-row justify-center items-center">
-            <Card />
-            <Card />
-            <Card />
+            { renderCards }
         </div>
     )
 }
