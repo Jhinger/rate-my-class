e4eb391c58107286ecb33c2fb21fe5a28bcf15c5
Jhinger <gurshanjhinger22@gmail.com>
2023-03-25 19:53:39 -0700

    refactor: remove uneccessary code and add profile routes

---
+++ pages/user/index.tsx
@@ -0,0 +1,11 @@
+// Check if user is signed in, if they are redirect to /user/id, otherwise redirect to home landing page.
+
+const Index = () => {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default Index;
