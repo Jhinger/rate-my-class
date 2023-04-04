1af803e0085e5db2c8c550c8f6a8c66cff28a20e
Jhinger <gurshanjhinger22@gmail.com>
2023-04-04 15:22:41 -0700

    refactor: update data fetching to handle new department table

--- components/Directory/directory.tsx
+++ components/Directory/directory.tsx
@@ -9,9 +9,9 @@ const Directory = ({ summary }: IDirectoryProps) => {
 
     const renderPanels = summary.map((department, index) => 
             <div key={index} className='w-full m-4 p-4 rounded-md bg-primary flex flex-row justify-between items-center ring-2 ring-primary ring-offset-2 ring-offset-secondary hover:ring-white hover:duration-150'>
-                <span className='font-extrabold ml-8'>{ department.department }</span>
+                <span className='font-extrabold ml-8'>{ department.name }</span>
                 <div className='flex flex-row justify-between w-[8rem] mr-16'>
-                    <span>{ getGrade(Math.ceil(department._avg.avgGrade)) ?? '-'}</span>
+                    <span>{ getGrade(Math.ceil(department.avgGrade)) ?? '-'}</span>
                     <span>{ department.numComments ?? '-'}</span>
                 </div>
             </div>
