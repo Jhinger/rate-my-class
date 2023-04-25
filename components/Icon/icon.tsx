697a08ea32882a09cf537a4a95d49bfc79316ac8
Jhinger <gurshanjhinger22@gmail.com>
2023-04-25 14:53:33 -0700

    refactor: update navbar icons

---
+++ components/Icon/icon.tsx
@@ -0,0 +1,20 @@
+import Link from "next/link";
+
+interface IIconProps {
+    icon: React.ReactNode;
+    href?: string;
+    className?: string;
+}
+
+const Icon = ({ icon, href, className }: IIconProps) => {
+    return (
+        <>
+            {href
+                ? <Link className={className} href={href}> { icon } </Link>
+                : <div className={className}> { icon } </div>
+            }
+        </>
+    )
+}
+
+export default Icon;
--- components/Navbar/Primary/primary.tsx
+++ components/Navbar/Primary/primary.tsx
@@ -1,13 +1,16 @@
 import Image from 'next/image';
 import { signIn, signOut, useSession } from 'next-auth/react';
+import Router from 'next/router';
+import Link from 'next/link';
 
 import logo from '@/static/logo.svg'
+import { GithubIcon, LinkedinIcon, InstagramIcon } from 'lucide-react';
 import Profile from '@/components/Button/Profile';
 import SignIn from '@/components/Button/SignIn';
 import LoadingSpinner from '@/components/LoadingSpinner';
+import Icon from '@/components/Icon';
 
 import type { StatusOptions } from '@/types';
-import Router from 'next/router';
 
 const PrimaryNavbar = () => {
     const { data: session, status } = useSession();
@@ -30,8 +33,6 @@ const PrimaryNavbar = () => {
         Router.push('/');
     }
 
-    const baseSocialURL = '/static/socials/';
-
     return (
         <div className="relative w-full mt-2 mb-2">
             <nav className="max-w-[2100px] flex justify-between center items-center">
@@ -45,12 +46,12 @@ const PrimaryNavbar = () => {
                 />
 
                 <div className="flex flex-row space-x-10 invisible md:visible">
-                    <Image className="unselectable cursor-pointer" src={baseSocialURL + 'github.svg'} width={28} height={25} alt={'GitHub'} priority={true} />
-                    <Image className="unselectable cursor-pointer" src={baseSocialURL + 'linkedin.svg'} width={28} height={25} alt={'LinkedIn'} priority={true} />
-                    <Image className="unselectable cursor-pointer" src={baseSocialURL + 'instagram.svg'} width={28} height={25} alt={'Instagram'} priority={true} />
+                    <Icon icon={<GithubIcon size={25}/>} href="#" className='unselectable text-primary hover:text-tertiary duration-150' />
+                    <Icon icon={<LinkedinIcon size={25}/>} href="#" className='unselectable text-primary hover:text-tertiary duration-150' />
+                    <Icon icon={<InstagramIcon size={25}/>} href="#" className='unselectable text-primary hover:text-tertiary duration-150' />
                 </div>
 
-                <ul className="flex flex-row space-x-10 text-primary invisible md:visible">
+                <ul className="flex flex-row space-x-10 text-primary font-medium invisible md:visible">
                     <li className="hover:text-tertiary duration-150 cursor-pointer">contact</li>
                     <li className="hover:text-tertiary duration-150 cursor-pointer">about</li>
                 </ul>
