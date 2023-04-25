188f7e8480a958ab4736fc9f6762e7233a43f562
Jhinger <gurshanjhinger22@gmail.com>
2023-04-24 22:11:33 -0700

    feat: add SEO component to each page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import Hero from '@/components/Hero'
-import Head from 'next/head'
 import Features from '@/components/Features'
+import SEO from '@/components/SEO'
 import Overview from '@/components/Overview'
 import prisma from '@/lib/prismadb'
 
@@ -13,12 +13,7 @@ export default function Home({ schools }: InferGetStaticPropsType<typeof getStat
 
 	return (
 		<>
-			<Head>
-				<title>RateMyClass</title>
-				<meta name="description" content="RateMyClass is an informational hub for University and College students looking for student-based feedback regarding their classes." />
-				<meta name="viewport" content="width=device-width, initial-scale=1" />
-				<link rel="icon" href="/static/logo-2.svg" sizes='16x16'/>
-			</Head>
+			<SEO />
 			<Hero schools={schools} />
 			<Features />
 			<Overview />
