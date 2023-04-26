a5834249055b909c1d0bd938406e548d5395a68d
Jhinger <gurshanjhinger22@gmail.com>
2023-04-26 16:44:53 -0700

    refactor: update schema, update sizing and boilerplate code

--- components/Button/Request/request.tsx
+++ components/Button/Request/request.tsx
@@ -18,16 +18,16 @@ const Request = () => {
 
     return (
         <div className="flex flex-col justify-center items-center absolute top-0" onClick={updateFormVisibility}>
-            <button id="request" className="font-semibold bg-primary rounded-md px-24 py-4 hover:bg-tertiary duration-300 w-max">
+            <button id="request" className="font-semibold text-sm bg-primary rounded-md px-24 py-4 hover:bg-tertiary duration-300 w-max">
                 REQUEST A SCHOOL / CLASS
             </button>
             {isOpen && 
                 <form onClick={stopPropagation} className="flex flex-col bg-primary p-2 m-2 w-full rounded-md">
-                    <label htmlFor="request-input" className="m-2 font-extralight text-sm">Enter a school and optionally a class:</label>
+                    <label htmlFor="request-input" className="m-2 text-xs font-normal">Enter a school and optionally a class:</label>
                     <input 
                         autoFocus
                         id="request-input" 
-                        className="rounded-sm p-2 m-2"
+                        className="rounded-sm p-2 m-2 ring-2"
                         type="text" 
                         placeholder="ex. Stanford - CS107" 
                         value={request} 
--- components/Directory/directory.tsx
+++ components/Directory/directory.tsx
@@ -8,7 +8,7 @@ interface IDirectoryProps {
 const Directory = ({ summary }: IDirectoryProps) => {
 
     const renderPanels = summary.map((department, index) => 
-            <div key={index} className='w-full m-4 p-4 rounded-md bg-primary flex flex-row justify-between items-center ring-2 ring-primary ring-offset-2 ring-offset-secondary hover:ring-tertiary hover:duration-150'>
+            <div key={index} className='w-full m-5 p-4 rounded-md bg-primary flex flex-row justify-between items-center ring-2 ring-primary ring-offset-2 ring-offset-secondary hover:ring-tertiary hover:duration-150'>
                 <span className='font-extrabold ml-8'>{ department.name }</span>
                 <div className='flex flex-row justify-between w-[8rem] mr-16'>
                     <span>{ getGrade(Math.ceil(department.avgGrade)) ?? '-'}</span>
