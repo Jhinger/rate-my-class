719332e2887f5689511742c754202e82e5e134aa
Jhinger <gurshanjhinger22@gmail.com>
2023-04-25 20:35:21 -0700

    feat: add progress bar, begin resizing of all components and text

--- components/Hero/hero.tsx
+++ components/Hero/hero.tsx
@@ -25,29 +25,29 @@ const Hero = ({
     const [userSchool, setUserSchool] = useState<UntypedObject>({});
     
     return (
-        <header className="w-full h-[40rem] flex flex-col justify-center items-center">
+        <header className="w-full h-[35rem] flex flex-col justify-center items-center">
             <div className="flex flex-col relative -top-10">
                 <div className="flex flex-col justify-center items-center">
                     <div className="flex flex-row items-center">
                         <Image
                             className="unselectable"
                             src={logo}
-                            width={40}
+                            width={35}
                             height={30}
                             quality={100}
                             alt=""
                             priority={true}
                         />
-                        <h1 className="p-2 font-extrabold text-8xl tracking-tightest bg-gradient-to-r bg-clip-text text-transparent from-primary via-tertiaryAccent to-tertiary animate-gradient"> 
+                        <h1 className="px-2 font-extrabold text-[5.5rem] tracking-tightest bg-gradient-to-r bg-clip-text text-transparent from-primary via-tertiaryAccent to-tertiary animate-gradient"> 
                             { title } 
                         </h1>
                     </div>
-                    <h4 className="text-white font-light mt-4"> { subtitle } </h4>
+                    <h5 className="text-white text-sm font-base"> { subtitle } </h5>
                 </div>
                 <div className="m-4 relative">
                     <SearchBar options={schools} setUserSelected={setUserSchool} placeholder={`ex. 'Simon Fraser University' or 'SFU'`} className="w-[60rem]" />
                 </div>
-                <div className="text-white text-sm font-extralight text-center">
+                <div className="text-white text-xs font-extralight text-center">
                     Can&lsquo;t find your school? Request it to be <a href='#request' className='text-blue-400 hover:text-tertiary hover:cursor-pointer'>added here</a>.
                 </div>
             </div>
