4d449bd052d3fc2b0aa3a5509480352190682a5d
Jhinger <gurshanjhinger22@gmail.com>
2023-04-25 21:08:20 -0700

    refactor: update profile and card sizing

--- components/Card/card.tsx
+++ components/Card/card.tsx
@@ -11,12 +11,12 @@ interface ICardProps {
 const Card = ({ imgSrc, title, subtext, className }: ICardProps) => {
     return (
         <Tilt tiltMaxAngleX={8} tiltMaxAngleY={8} tiltReverse={true}>
-            <div className={`w-[25rem] bg-gradient-to-br from-tertiary to-tertiaryComplement aspect-square flex justify-center items-center mx-6 rounded-3xl animate-gradient`}>
+            <div className={`w-[23rem] bg-gradient-to-br from-tertiary to-tertiaryComplement aspect-square flex justify-center items-center mx-6 rounded-3xl animate-gradient`}>
                 <div className={`${className} aspect-square rounded-3xl m-2 flex flex-col justify-center items-center`}>
                     <Image 
                         src={imgSrc}
-                        width={175}
-                        height={175}
+                        width={125}
+                        height={125}
                         quality={100}
                         priority={true}
                         unoptimized
@@ -24,8 +24,8 @@ const Card = ({ imgSrc, title, subtext, className }: ICardProps) => {
                         className="unselectable"
                     />
                     <div className="flex flex-col justify-center items-center mt-6">
-                        <h5 className="font-bold text-2xl tracking-tight text-secondary">{ title }</h5>
-                        <p className="mx-8 text-center my-2 font-light text-[0.9rem]">{ subtext }</p>
+                        <h2 className="font-bold text-2xl tracking-tight text-secondary">{ title }</h2>
+                        <p className="mx-7 text-center text-sm my-2 font-light text-[0.9rem]">{ subtext }</p>
                     </div>
                 </div>
             </div>
