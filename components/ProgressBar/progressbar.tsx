addbc6807153c061ce710f0034fb21e2b41e8df1
Jhinger <gurshanjhinger22@gmail.com>
2023-04-27 18:09:42 -0700

    refactor: update progress bar to handle non standard percentages

--- components/ProgressBar/progressbar.tsx
+++ components/ProgressBar/progressbar.tsx
@@ -1,18 +1,22 @@
 
 interface IProgressBarProps {
-    percent?: number;
-    color?: string;
+    percent: number;
+    maxPercent: number;
+    color: string;
+    textColor: string;
     className?: string;
 }
 
-const ProgressBar = ({ percent = 20, color = "primary", className }: IProgressBarProps) => {
+const ProgressBar = ({ percent = 20, maxPercent = 100, color = "bg-primary", textColor, className }: IProgressBarProps) => {
+    const progress = (percent / maxPercent) * 100;
+
     return (
         <div className={`${className} flex flex-row h-8 max-w-min justify-center items-center`}>
             <div className={`relative h-3 w-[10rem] bg-gray-500 rounded-md`} >
-                <div className={`absolute top-0 left-0 h-3 rounded-md bg-primary`} style={{width: `${percent}%`}} />
+                <div className={`absolute top-0 left-0 h-3 rounded-md ${color}`} style={{width: `${progress}%`}} />
             </div>
             <div className="flex justify-center items-center mx-1">
-                <p className="font-bold leading-none text-xs text-primary">{percent}%</p>
+                <p className={`${textColor} font-bold leading-none text-xs`}>{progress}%</p>
             </div>
         </div>
     )
