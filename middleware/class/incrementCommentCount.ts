67fc73cc068ae21c2bbaa40e8b6df75fa177bf50
Jhinger <gurshanjhinger22@gmail.com>
2023-04-30 21:22:21 -0700

    fix: grade distribution graph

--- middleware/class/incrementCommentCount.ts
+++ middleware/class/incrementCommentCount.ts
@@ -1,6 +1,6 @@
 import prisma from '@/lib/prismadb'
 
-prisma.$use(async (params, next) => {
+export default prisma.$use(async (params, next) => {
     if (params.model === 'Comment' && params.action === 'create') {
         const classID = params.args.data.classId;
         await prisma.class.update({
