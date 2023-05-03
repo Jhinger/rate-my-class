3476088a7c559ff27d5b58a5b2104f4a0a8b5338
Jhinger <gurshanjhinger22@gmail.com>
2023-05-02 19:33:59 -0700

    feat: more class page changes

---
+++ components/Comment/comment.tsx
@@ -0,0 +1,14 @@
+
+interface ICommentProps {
+    comment: any;
+}
+
+const Comment = ({ comment }: ICommentProps) => {
+    return (
+        <div className="bg-primary rounded-md w-[50rem] h-[22rem]">
+
+        </div>
+    )
+}
+
+export default Comment;
--- util/getGrade.ts
+++ util/getGrade.ts
@@ -1,5 +1,5 @@
 
-export default function getGrade(num: number): string {
+function getGrade(num: number): string {
     const map = {
         1: 'F',
         2: 'D',
@@ -15,4 +15,28 @@ export default function getGrade(num: number): string {
     }
 
     return map[num as keyof typeof map] ?? 'Unknown';
+}
+
+function getNumberGrade(num: number = 6): number {
+    const number = Math.round(num);
+    const map = {
+        11: 98.5,
+        10: 92.5,
+        9: 88.5,
+        8: 82.5,
+        7: 78.5,
+        6: 72.5,
+        5: 68.5,
+        4: 62.5,
+        3: 58.5,
+        2: 52.5,
+        1: 48.5
+    }
+
+    return map[number as keyof typeof map];
+}
+
+export {
+    getGrade,
+    getNumberGrade
 }
