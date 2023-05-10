f2bdc58ebcd60e6542a08882349f0c903d2ff9af
Jhinger <gurshanjhinger22@gmail.com>
2023-05-09 19:40:27 -0700

    refactor: migrate class page - some small errors with fetching data

--- app/[school]/[class]/page.tsx
+++ app/[school]/[class]/page.tsx
@@ -6,6 +6,16 @@ import {
   	getGradeDistribution
 } from './actions';
 import { Metadata } from "next"
+import ClassHeader from '@/components/ClassHeader';
+import ClassSummary from '@/components/ClassSummary';
+import CallToAction from '@/components/Button/CallToAction';
+import Chart from '@/components/Chart';
+import Comment from '@/components/Comment';
+import CommentsContainer from '@/components/CommentsContainer';
+import CommentOptionsContainer from '@/components/CommentOptionsContainer';
+import { colors_blue } from '@/constants/boosterColors';
+
+import type { Comment as CommentType } from '@prisma/client';
 
 export async function generateMetadata({ params }: { params: { _class: string; school: string } }): Promise<Metadata | undefined> {
     const { _class, school } = params;
@@ -43,7 +53,43 @@ export default async function ClassPage({ params }: { params: { _class: string; 
 
     return (
         <div>
-            Class Page
+            <ClassHeader className="w-full h-full flex justify-center pt-12 pb-24">
+                <div className="flex flex-row w-max h-full items-start relative left-12">
+                    <div className="relative top-2">
+                        <ClassSummary 
+                            averages={averages}
+                            school={{'name': _school!.name!, 'short': _school!.short!}}
+                            numComments={_class_!.numComments!}
+                            className={_class_!.name!}
+                        />
+                        <div className="flex flex-row gap-x-4 justify-center items-start w-max">
+                            <CallToAction className="bg-green-300 h-10">
+                                Rate
+                            </CallToAction>
+                            <CallToAction className="bg-blue-300 h-10">
+                                Share
+                            </CallToAction>
+                        </div>
+                    </div>
+                    <div className="min-w-min flex gap-8 justify-center items-center flex-row">
+                        <Chart type="barchart" classes="w-[40rem] h-max" data={distribution} label={`${_class_!.name} Grade Distribution`} colors={colors_blue} />
+                        <Chart type="piechart" classes="w-[24rem] relative bottom-4 bg-transparent" pieData={averages?.avgBooster ?? 0} />
+                    </div>
+                </div>
+            </ClassHeader>
+            <CommentsContainer className="bg-secondary p-16 flex flex-col justify-center items-center text-white w-full">
+                <CommentOptionsContainer className="flex flex-row justify-end mb-4 w-[50rem] border-2 border-solid border-blue-500">
+                    <CallToAction className="bg-tertiary text-black h-[2.5rem]">Filter</CallToAction>
+
+                </CommentOptionsContainer>
+                <div className="flex flex-col gap-y-12">
+                    {comments.map((comment: CommentType, index: number) => 
+                        <div key={index}>
+                            <Comment comment={comment} />
+                        </div>
+                    )}
+                </div>
+            </CommentsContainer>
         </div>
     )
 }
