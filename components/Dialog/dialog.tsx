ce6153860eb4b98fd2b8df6ee86c0dfed29ca088
Jhinger <gurshanjhinger22@gmail.com>
2023-05-16 14:36:19 -0700

    feat: dialog component container setup

--- components/Dialog/dialog.tsx
+++ components/Dialog/dialog.tsx
@@ -8,7 +8,7 @@ interface IDialogProps {
 }
 
 const Dialog = ({ children }: IDialogProps) => {
-    const [dialogOpen, setIsDialogOpen] = useState(false);
+    const [dialogOpen, setIsDialogOpen] = useState(true);
     const ref = useRef<HTMLDivElement>(null);
 
     const closeDialog = () => {
@@ -18,12 +18,17 @@ const Dialog = ({ children }: IDialogProps) => {
     useOnClickOutside(ref, closeDialog);
 
     return (
-        <div className="absolute top-0 bottom-0 w-full h-screen z-10 flex justify-center items-center border-solid border-2 border-red-500">
-            <div className="absolute w-full h-full bg-black opacity-50" />
-            <div className="sticky w-[60rem] h-[40rem] bg-tertiary rounded-md">
-                { children }
-            </div>
-        </div>
+        <>
+            {dialogOpen 
+                ? <div className="fixed top-0 bottom-0 w-full h-screen z-10 flex justify-center items-center">
+                    <div className="absolute w-full h-full bg-black opacity-50" />
+                    <div ref={ref} className="sticky w-[60rem] h-[40rem] bg-tertiary rounded-md">
+                        { children }
+                    </div>
+                </div>
+                : null
+            }
+        </>
     )
 }
 
