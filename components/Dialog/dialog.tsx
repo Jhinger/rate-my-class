82a5249c5ef9b40e20effc64aed63c25c0026a4a
Jhinger <gurshanjhinger22@gmail.com>
2023-05-18 19:09:09 -0700

    refactor: add overall rating score to comments

--- components/Dialog/dialog.tsx
+++ components/Dialog/dialog.tsx
@@ -20,12 +20,13 @@ const Dialog = ({ children }: IDialogProps) => {
     return (
         <>
             {dialogOpen 
-                ? <div className="fixed top-0 bottom-0 w-full h-screen z-10 flex justify-center items-center">
-                    <div className="absolute w-full h-full bg-black opacity-50" />
-                    <div ref={ref} className="sticky w-[60rem] h-[40rem] bg-tertiary rounded-md">
-                        { children }
+                ?
+                    <div className="fixed top-0 bottom-0 w-full h-screen z-10 flex justify-center items-center">
+                        <div className="absolute w-full h-full bg-black opacity-50" />
+                        <div ref={ref} className="sticky w-[60rem] h-[40rem] bg-tertiary rounded-md">
+                            { children }
+                        </div>
                     </div>
-                </div>
                 : null
             }
         </>
