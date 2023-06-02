14bda3719639d351683e8d5c72fbdbeb942244e8
Jhinger <gurshanjhinger22@gmail.com>
2023-06-01 21:51:31 -0700

    feat: added tag component

---
+++ components/Tag/tag.tsx
@@ -0,0 +1,15 @@
+
+interface ITagProps {
+    className: string;
+    children: React.ReactNode;
+}
+
+const Tag = ({ className, children }: ITagProps) => {
+    return (
+        <div className={`${className} rounded-sm flex justify-center items-center text-xs font-semibold px-2 py-1`}>
+            { children }
+        </div>
+    )
+}
+
+export default Tag;
