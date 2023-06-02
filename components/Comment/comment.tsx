7dc7ca16ddfe1e7ec8b31cdf19f1320bc514054d
Jhinger <gurshanjhinger22@gmail.com>
2023-06-01 21:51:31 -0700

    feat: added tag component

--- components/Comment/comment.tsx
+++ components/Comment/comment.tsx
@@ -1,5 +1,7 @@
 import getDate from '@/util/getDate';
 import Icon from '@/components/Button/Icon';
+import Tag from '@/components/Tag';
+import getTagName from '@/util/getTagName';
 import { MoreVerticalIcon } from 'lucide-react';
 
 import type { Comment as CommentType } from '@prisma/client';
@@ -11,20 +13,28 @@ interface ICommentProps {
 const Comment = ({ comment }: ICommentProps) => {
     const commentDate = getDate(comment.createdAt);
 
+    const renderTags = comment.tags.map((tag, index) =>
+        <div key={index}>
+            <Tag className='bg-white mx-2'>
+                { getTagName(tag) }
+            </Tag>
+        </div>
+    )
+
     return (
-        <div className="bg-gray-200 text-black text-base font-semibold rounded-md w-[60rem] h-[25rem]">
-            <div className='w-full flex relative top-4 justify-between items-center px-8'> 
+        <div className="bg-gray-200 text-black flex flex-col text-base font-semibold rounded-md w-[60rem] h-[25rem]">
+            <div className='w-full flex mt-4 justify-between items-center px-8'> 
                 { commentDate }
                 <Icon>
                     <MoreVerticalIcon size={20} />
                 </Icon>
             </div>
-            <div className='container for left box and right'>
+            <div className='w-full h-full border-2 border-solid border-red-500'>
                 <div className='container left box'>
 
                 </div>
                 <div className='container right box'>
-                    
+                    <div className='flex flex-row'> { renderTags } </div>
                 </div>
             </div>
         </div>
