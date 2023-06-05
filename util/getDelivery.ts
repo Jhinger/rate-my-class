9ab8a6261cfcd5068d262d6c13c226b6b6418f5b
Jhinger <gurshanjhinger22@gmail.com>
2023-06-04 21:16:24 -0700

    feat: update comment to hold proper informational tags

---
+++ util/getDelivery.ts
@@ -0,0 +1,10 @@
+import type { DELIVERY } from "@prisma/client";
+
+export default function getDelivery(delivery: DELIVERY) {
+    const map = {
+        "INPERSON": "In Person",
+        "ONLINE": "Online"
+    }
+
+    return map[delivery];
+}
