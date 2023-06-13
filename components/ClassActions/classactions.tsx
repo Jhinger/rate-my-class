86fa468347cc0ced73b4a47d2b9d0c4af8a3df4e
Jhinger <gurshanjhinger22@gmail.com>
2023-06-13 11:26:21 -0700

    feat: update loading screens, setup dialog component for comment form

--- components/ClassActions/classactions.tsx
+++ components/ClassActions/classactions.tsx
@@ -2,16 +2,29 @@
 
 import CallToAction from "@/components/Button/CallToAction";
 import Dialog from "@/components/Dialog";
+import CommentForm from "@/components/CommentForm";
+import { useState } from "react";
 
 const ClassActions = () => {
+    const [dialogOpen, setIsDialogOpen] = useState(true);
+
+    const openDialog = () => {
+        setIsDialogOpen(true);
+    }
+
     return (
-        <div className="flex flex-row gap-x-4 justify-center items-start w-max">
-            <CallToAction className="bg-green-300 h-10">
-                Rate
-            </CallToAction>
-            <CallToAction className="bg-blue-300 h-10">
-                Share
-            </CallToAction>
+        <div className="">
+            <Dialog isOpen={dialogOpen} setIsOpen={setIsDialogOpen}>
+                
+            </Dialog>
+            <div className="flex flex-row gap-x-4 justify-center items-start w-max">
+                <CallToAction onClick={openDialog} className="bg-green-300 h-10">
+                    Rate
+                </CallToAction>
+                <CallToAction className="bg-blue-300 h-10">
+                    Share
+                </CallToAction>
+            </div>
         </div>
     )
 }
