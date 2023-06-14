8f68b98722138fb67493dbe135d7adf723dcdc53
Jhinger <gurshanjhinger22@gmail.com>
2023-06-14 13:01:48 -0700

    feat: clipboard functionality

--- components/ClassActions/classactions.tsx
+++ components/ClassActions/classactions.tsx
@@ -3,26 +3,37 @@
 import CallToAction from "@/components/Button/CallToAction";
 import Dialog from "@/components/Dialog";
 import CommentForm from "@/components/CommentForm";
+import { Clipboard, ClipboardCheck } from "lucide-react";
 import { useState } from "react";
 
 const ClassActions = () => {
     const [dialogOpen, setIsDialogOpen] = useState(false);
+    const [copied, setIsCopied] = useState(false);
 
     const openDialog = () => {
         setIsDialogOpen(true);
     }
 
+    const copyToClipboard = async () => {
+        await navigator.clipboard.writeText(window.location.href);
+        setIsCopied(true);
+    }
+
     return (
-        <div className="">
+        <div>
             <Dialog isOpen={dialogOpen} setIsOpen={setIsDialogOpen}>
-                
+                <CommentForm />
             </Dialog>
             <div className="flex flex-row gap-x-4 justify-center items-start w-max">
                 <CallToAction onClick={openDialog} className="bg-green-300 h-10">
                     Rate
                 </CallToAction>
-                <CallToAction className="bg-blue-300 h-10">
-                    Share
+                <CallToAction onClick={copyToClipboard} className="bg-blue-300 h-10">
+                    {copied 
+                        ? <ClipboardCheck size={15} className="" />
+                        : <Clipboard size={15} className="bg-white-500" />
+                    }
+                    <span className="mx-2">{copied ? 'Copied' : 'Share'}</span>
                 </CallToAction>
             </div>
         </div>
