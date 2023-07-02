ca34e981016527cb5c85d42ee070a9e5daf52e18
Jhinger <gurshanjhinger22@gmail.com>
2023-07-02 12:33:10 -0700

    feat: add scrolling dialog component

--- components/CommentForm/actions.ts
+++ components/CommentForm/actions.ts
@@ -1,9 +1,12 @@
 "use server";
 
 import prisma from '@/lib/prismadb';
+import { revalidatePath } from 'next/cache';
 
 const onSubmit = (form: FormData) => {
+    
     console.log(form);
+    // revalidatePath('/[school]/[class]');
 }
 
 export {
