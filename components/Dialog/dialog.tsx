0c32f9914e67d0ce45b2587ed76c0f95f018af31
Jhinger <gurshanjhinger22@gmail.com>
2023-07-02 12:33:10 -0700

    feat: add scrolling dialog component

--- components/Dialog/dialog.tsx
+++ components/Dialog/dialog.tsx
@@ -22,9 +22,9 @@ const Dialog = ({ isOpen, setIsOpen, children }: IDialogProps) => {
         <>
             {isOpen 
                 ?
-                    <div className="fixed top-0 bottom-0 left-0 right-0 w-full h-full z-10 flex justify-center items-center">
+                    <div className="fixed top-0 bottom-0 left-0 right-0 p-10 w-full h-full z-10 flex justify-center items-center">
                         <div className="absolute w-full h-full bg-black opacity-50" />
-                        <div ref={ref} className="sticky w-min bg-gray-300 rounded-md p-12">
+                        <div ref={ref} className="sticky w-min bg-gray-300 rounded-md p-12 h-full overflow-y-scroll no-scrollbar">
                             { children }
                         </div>
                     </div>
