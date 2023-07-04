1c77b7e11179f5cecf522c6767b4c1263b1d1184
Jhinger <gurshanjhinger22@gmail.com>
2023-07-03 18:49:16 -0700

    feat: setup api routes as server actions incompatible with validation libraries

--- components/CommentForm/actions.ts
+++ components/CommentForm/actions.ts
@@ -2,13 +2,15 @@
 
 import prisma from '@/lib/prismadb';
 import { revalidatePath } from 'next/cache';
+import type { FieldValues, SubmitHandler } from 'react-hook-form';
 
-const onSubmit = (form: FormData) => {
-    
-    console.log(form);
-    // revalidatePath('/[school]/[class]');
+export interface IFormValues {
+    tags: string[];
 }
 
-export {
-    onSubmit
+export async function onSubmit(form: IFormValues) {
+    
+    console.log(form);
+    // const tags = form.getAll("tags");
+    // revalidatePath('/[school]/[class]');
 }
