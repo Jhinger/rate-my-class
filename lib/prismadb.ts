e04a9e05772cf2eb2a957bb89685ec48ca8e501b
Jhinger <gurshanjhinger22@gmail.com>
2023-07-09 23:19:03 -0700

    refactor: incrementCommentCount prisma middleware

--- lib/prismadb.ts
+++ lib/prismadb.ts
@@ -1,10 +1,14 @@
-import { PrismaClient } from "@prisma/client"
+import { PrismaClient } from "@prisma/client";
+import incrementCommentCount from '@/middleware/class/incrementCommentCount';
 
 declare global {
   var prisma: PrismaClient | undefined;
 }
 
 const prisma = globalThis.prisma || new PrismaClient();
+
+prisma.$use(incrementCommentCount);
+
 if (process.env.NODE_ENV !== "production") globalThis.prisma = prisma;
 
 export default prisma;
--- middleware/class/incrementCommentCount.ts
+++ middleware/class/incrementCommentCount.ts
@@ -1,8 +1,12 @@
 import prisma from '@/lib/prismadb'
+import type { Prisma } from '@prisma/client';
 
-export default prisma.$use(async (params, next) => {
+export default async function incrementCommentCount(
+        params: Prisma.MiddlewareParams, 
+        next: (params: Prisma.MiddlewareParams) => Promise<any>
+    ) {
     if (params.model === 'Comment' && params.action === 'create') {
-        const classID = params.args.data.classId;
+        const classID = params.args.data.class.connect.id;
         await prisma.class.update({
             where: {
                 id: classID
@@ -16,7 +20,7 @@ export default prisma.$use(async (params, next) => {
     }
 
     return next(params);
-})
+}
 
 // Check if grade is provided, otherwise we might not want to increment the number of comments.
 // If we have to increment for other averages, we should run the update average functions before
