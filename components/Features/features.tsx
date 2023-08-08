f3ebf40b0cd1b5c61f14b5c3ad682913c2c03cff
Jhinger <gurshanjhinger22@gmail.com>
2023-08-08 11:18:08 -0700

    refactor: map cards

--- components/Features/features.tsx
+++ components/Features/features.tsx
@@ -2,28 +2,17 @@ import Card from "@/components/Card";
 import { cards } from "@/util/getCardProps";
 
 const Features = () => {
-	const [card1, card2, card3] = cards;
-
 	return (
 		<div className="flex h-max w-full flex-col items-center justify-center gap-x-4 gap-y-8 bg-secondary py-16 sm:flex-row sm:flex-wrap">
-			<Card
-				imgSrc={card1.imgSrc}
-				title={card1.title}
-				subtext={card1.subtext}
-				className="bg-primary"
-			/>
-			<Card
-				imgSrc={card2.imgSrc}
-				title={card2.title}
-				subtext={card2.subtext}
-				className="bg-primary"
-			/>
-			<Card
-				imgSrc={card3.imgSrc}
-				title={card3.title}
-				subtext={card3.subtext}
-				className="bg-primary"
-			/>
+			{...cards.map((card, key) => (
+				<Card
+					key={key}
+					imgSrc={card.imgSrc}
+					title={card.title}
+					subtext={card.subtext}
+					className="bg-primary"
+				/>
+			))}
 		</div>
 	);
 };
